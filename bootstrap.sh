b943513614c15b9092206f694b8f96622de44df7
Michał Roszka <michal@roszka.pl>
2015-09-24 03:55:20 +0200

    stop and disable all Varnish services
    Stop and disable the following services:
    
    * the Varnish daemon
    * the Varnish logging daemon
    * the Varnish NCSA logging daemon

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,15 @@ apt-get update
 # Install Varnish and its documentation.
 apt-get install -y varnish varnish-doc
 
+# Stop and disable the Varnish daemon.
+systemctl stop varnish.service
+systemctl disable varnish.service
+
+# Stop and disable Varnish logging daemon.
+systemctl stop varnishlog.service
+systemctl disable varnishlog.service
+
+# Stop and disable Varnish NCSA logging daemon.
+systemctl stop varnishncsa.service
+systemctl disable varnishncsa.service
+
